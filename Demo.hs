692a1bf04f7e232a0c7394a9215d593af6e08487
Jean-Baptiste Mazon <jmazon@ncejmazon-lnx.nce.amadeus.net>
2012-11-15 17:30:37 +0100

    Rewrite and comment up to (not including) typeclasses

--- Demo.hs
+++ Demo.hs
@@ -1,23 +1,31 @@
+-- Gloss over this for now
 import WaveExport
 
-type Freq = Float
+-- Accoustics basics
 type Sample = Float
 type Wave = [Sample]
-
 rate = 44100
 
-indexToPhase n = 2*pi * n / rate
+-- Sine wave generation
+type Freq = Float
+indexToPhase n = 2*pi * n / rate          -- voluntarily no signature
 makeSample f n = sin(f * indexToPhase(n)) -- expressions can look like C
-sineWave :: Freq -> Wave
-sineWave f = map (makeSample f) [0..]     -- partial application
 
+sineWave :: Freq -> Wave
+sineWave f = map (makeSample f) [0..]
+-- Important: function definitions
+--            recursive (potentially infinite structure)
+-- Optional: partial application
+
+-- > dump (sineWave 440)
 -- $ aplay -t raw -r 44100 -f FLOAT_LE audiodump
 
 data BPitch = La  | SiB | Si | Do  | DoD | Re
             | MiB | Mi  | Fa | FaD | Sol | LaB | La'
-              deriving Enum
+              deriving (Eq,Show,Enum) -- we'll speak about this line later
+-- Important: Data Type
 
--- take 13 $ iterate (* 2 ** (1/12)) 440
+-- (initially generated with: take 13 $ iterate (* 2 ** (1/12)) 440)
 pitchToFreq :: BPitch -> Freq
 pitchToFreq La  = 440.0
 pitchToFreq SiB = 466.1637615180899
@@ -32,6 +40,21 @@ pitchToFreq FaD = 739.988845423269
 pitchToFreq Sol = 783.9908719634989
 pitchToFreq LaB = 830.6093951598906
 pitchToFreq La' = 880.0000000000003
+-- Important: pattern matching
+
+-- Play a scale
+bpScale :: [BPitch]
+bpScale = [La,Si,DoD,Re,Mi,FaD,LaB,La']
+-- (I *know* about enharmonics, but not now!)
+
+playFreq :: Freq -> Wave
+playFreq f = take 22050 (sineWave f)
+
+playBPitch :: BPitch -> Wave
+playBPitch p = take 22050 $ sineWave $ pitchToFreq p
+
+playBPList :: [BPitch] -> Wave
+playBPList = concatMap playBPitch
 
 -- pitchToFreq' :: BPitch -> Freq
 pitchToFreq' p = 440 * 2 ** (n/12)
