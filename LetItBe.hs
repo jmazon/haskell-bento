f6611e5afb9dab4118c1bb23def1e9a011366cdb
Jean-Baptiste Mazon <jmazon@ncejmazon-lnx.nce.amadeus.net>
2012-11-16 15:04:53 +0100

    Update Let It Be to the latest and greatest notation

--- LetItBe.hs
+++ LetItBe.hs
@@ -1,7 +1,7 @@
 module LetItBe (letItBe) where
 
-letItBe = "3g4 \
-          \3g6 3g2 4a4 3e4 3g4 3g4 4c4 4d4 \
-          \4d4 4e4 4e4 4d4 4d4 4cc \
-          \4e4 4e4 4f4 4e2 4e4 4d8 4e2 4d2 \
-          \4d4 4cf"
+letItBe = "4g3 \
+          \6g3 2g3 4a4 4e3 4g3 4g3 4c4 4d4 \
+          \4d4 4e4 4e4 4d4 4d4 cc4 \
+          \4e4 4e4 4f4 2e4 4e4 8d4 2e4 2d4 \
+          \4d4 fc4"
