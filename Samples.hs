2e19b91a1684ec991ff615b749e4278195c9dcc2
Jean-Baptiste Mazon <jb@darkstar>
2012-11-17 00:18:34 +0100

    More canon!

--- Samples.hs
+++ Samples.hs
@@ -8,7 +8,7 @@ letItBe = "4g3 \
           \4d4 fc4"
 
 -- a famous tune by a 17th-century bavarian
--- Many thanks to Emmanuel for typing this in!
+-- Many thanks to Emmanuel for typing the first half in!
 kanon = "8F4 8e4 8d4 8C4 " ++
         "8b4 8a4 8b4 8C4 " ++
         "8d4 8C4 8b4 8a4 " ++
@@ -36,4 +36,38 @@ kanon = "8F4 8e4 8d4 8C4 " ++
         "4d4 4__ 4C4 4__ 4b4 4__ 4d4 4__ " ++
         "4d3 4__ 4d3 4__ 4d3 4__ 4e3 4__ " ++
         "4__ 4a4 4__ 4a4 4__ 4F3 4__ 4a4 " ++
-        "4__ 4g3 4__ 4F3 4__ 4g3 4__ 4e4"
+        "4__ 4g3 4__ 4F3 4__ 4g3 4__ 4e4 " ++
+        "2F4 2F3 2g3 2F3 2e3 2e4 2F4 2e4 2d4 2F3 2d3 2b4 2a4 2a3 2g2 2a3 " ++
+        "2b3 2b4 2C4 2b4 2a4 2a3 2g2 2a3 2b3 2b4 2a4 2b4 2C4 2C3 2b3 2C3 " ++
+        "2d3 2d4 2e4 2d4 2C4 2C3 2d3 2C3 2b3 2b4 2a4 2b4 2C4 2C3 2F3 2e3 " ++
+        "2d3 2d4 2e4 2g4 2F4 2F3 2a4 2F4 2d4 2g4 2F4 2g4 2e4 2a4 2g3 2a4 " ++
+        "2F3 2a4 2a4 2a4 2a4 2a4 2a4 2a4 2F3 2F3 2F3 2F3 2F3 2F3 2a4 2a4 " ++
+        "2g3 2g3 2g3 2d4 2d4 2d4 2d4 2d4 2d4 2d4 2b4 2b4 2a4 2a4 2e4 2C4 " ++
+        "2a4 2F4 2F4 2F4 2e4 2e4 2e4 2e4 2d4 2d4 2d4 2d4 2a5 2a5 2a5 2a5 " ++
+        "2b5 2b5 2b5 2b5 2a5 2a5 2a5 2a5 2b5 2b5 2b5 2b5 2C5 2C4 2C4 2C4 " ++
+        "2d4 1d3 1e3 2F3 2d3 2C3 1C4 1d4 2e4 2C4 \
+        \2b4 1b3 1C3 2d3 2b3 2C3 1a4 1g3 2F3 2e3 " ++
+        "2d3 1g3 1F3 2e3 2g3 2F3 1d3 1e3 2F3 2a4 \
+        \2g3 1b4 1a4 2g3 2F3 2e3 1a4 1g3 2F3 2e3 " ++
+        "2F3 1d4 1C4 2d4 2F3 2a4 1a4 1b4 2C4 2a4 \
+        \2F3 1d4 1e4 2F4 2d4 2F4 1F4 1e4 2d4 2C4 " ++
+        "2b4 1b4 1a4 2b4 2C4 2d4 1F4 1e4 2d4 2F4 \
+        \2g4 1d4 1C4 2b4 2b4 2a4 2e3 2a4 2a4 " ++
+        "ca4 4a4 cd3 4a4 " ++
+        "8g3 8a4 4g3 4d3 6d3 2C3 " ++
+        "4d3 4d4 8C4 8b4 8a4 " ++
+        "6d3 2e3 8F3 8b4 6e3 2e3 " ++
+        "6F3 2F4 2F4 2g4 2F4 2e4 6d4 2d4 2d4 2e4 2d4 2C4 " ++
+        "8b4 8d4 2d4 2c4 2b4 2c4 6a4 2a4 " ++
+        "6a4 2a5 2a5 2b5 2a5 2g4 6F4 2F4 2F4 2g4 2F4 2e4 " ++
+        "2d4 2c4 2b4 2c4 6a4 2a4 4g3 4d4 6C4 2C4 " ++
+        "4d4 8d4 8C4 8b4 8a4 8g3 aF3 2e3 8e3 " ++ -- (two measures here)
+        "4F3 8F4 4e4 4d4 8d5 4c5 " ++
+        "8b5 4d5 4a5 8b5 8a5 " ++
+        "8a5 6a4 2g3 8F3 6F4 2e4 " ++
+        "cd4 4d4 8d4 8C4 " ++
+        "4d4 4d3 4C3 4C4 4b4 4b3 4a3 4a4 " ++
+        "4g3 4g4 4F4 4F3 4e3 4b4 4e3 4e4 " ++
+        "4F4 4F3 4e3 4e4 4d4 4d3 4C3 4C4 " ++
+        "4b4 4b5 4a5 4a4 6g3 2e4 4a4 4a4 " ++
+        "4a4"
