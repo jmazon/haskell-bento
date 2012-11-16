a79b0f45c7cf7c21d6c5dbb961064c5aa690cff4
Jean-Baptiste Mazon <jmazon@ncejmazon-lnx.nce.amadeus.net>
2012-11-16 14:49:46 +0100

    Let it be

---
+++ LetItBe.hs
@@ -0,0 +1,7 @@
+module LetItBe (letItBe) where
+
+letItBe = "3g4 \
+          \3g6 3g2 4a4 3e4 3g4 3g4 4c4 4d4 \
+          \4d4 4e4 4e4 4d4 4d4 4cc \
+          \4e4 4e4 4f4 4e2 4e4 4d8 4e2 4d2 \
+          \4d4 4cf"
