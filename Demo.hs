89074595e3544c2780f3f0ea3a7a17f69d04ddef
Jean-Baptiste Mazon <jmazon@ncejmazon-lnx.nce.amadeus.net>
2012-11-16 20:15:05 +0100

    Move all music samples to a single file

--- Demo.hs
+++ Demo.hs
@@ -3,7 +3,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 import WaveExport
 import Data.Char
-import LetItBe
+import Samples
 
 -- Accoustics basics
 type Sample = Float
@@ -174,31 +174,6 @@ readEvent _ = Nothing
 -- Ewww… hard to tell what this one demonstrates without uttering the M-word ;-)
 readScore :: String -> Maybe [Event]
 readScore = mapM readEvent . words
-
-kanon = "8F4 8e4 8d4 8C4 " ++
-        "8b4 8a4 8b4 8C4 " ++
-        "8d4 8C4 8b4 8a4 " ++
-        "8g3 8F3 8g3 8e3 " ++
-        "4d3 4F3 4a4 4g3 4F3 4d3 4F3 4e3 " ++
-        "4d3 4b3 4d3 4a4 4g3 4b4 4a4 4g3 " ++
-        "4F3 4d3 4e3 4C4 4d4 4F4 4a5 4a4 " ++
-        "4b4 4g3 4a4 4F3 4d3 4d4 6d4 2C4 " ++
-        "2d4 2C4 2d4 2d3 2C3 2a4 2e3 2F3 2d3 2d4 2C4 2b4 2C4 2F4 2a5 2b5 " ++
-        "2g4 2F4 2e4 2g4 2F4 2e4 2d4 2C4 2b4 2a4 2g3 2F3 2e3 2g3 2F3 2e3 " ++
-        "2d3 2e3 2F3 2g3 2a4 2e3 2a4 2g3 2F3 2b4 2a4 2g3 2a4 2g3 2F3 2e3 " ++
-        "2d3 2b3 2b4 2C4 2d4 2C4 2b4 2a4 2g3 2F3 2e3 2b4 2a4 2b4 2a4 2g3 " ++
-        "4F3 4F4 8e4 4__ 4d4 8F4 " ++
-        "8b5 8a5 8b5 8C5 " ++
-        "4d5 4d4 8C4 4__ 4b4 8d4 " ++
-        "Cd4 4d4 4d4 4g4 4e4 4a5 " ++
-        "2a5 1F4 1g4 2a5 1F4 1g4 1a5 1a4 1b4 1C4 1d4 1e4 1F4 1g4 2F4 1d4 1e4 2F4 1F3 1g3 1a4 1b4 1a4 1g3 1a4 1F3 1g3 1a4 " ++
-        "2g3 1b4 1a4 2g3 1F3 1e3 1F3 1e3 1d3 1e3 1F3 1g3 1a4 1b4 2g3 1b4 1a4 2b4 1C4 1d4 1a4 1b4 1C4 1d4 1e4 1F4 1g4 1a5 " ++
-        "2F4 1d4 1e4 2F4 1e4 1d4 1e4 1C4 1d4 1e4 1F4 1e4 1d4 1C4 2d4 1b4 1C4 2d4 1d3 1e3 1F3 1g3 1F3 1e3 1F3 1d4 1C4 1d4 " ++
-        "2b4 1d4 1C4 2b4 1a4 1g3 1a4 1g3 1F3 1g3 1a4 1b4 1C4 1d4 2b4 1d4 1C4 2d4 1C4 1b4 1C4 1d4 1e4 1d4 1C4 1d4 1b4 1C4 " ++
-        "4d4 4__ 4C4 4__ 4b4 4__ 4d4 4__ " ++
-        "4d3 4__ 4d3 4__ 4d3 4__ 4e3 4__ " ++
-        "4__ 4a4 4__ 4a4 4__ 4F3 4__ 4a4 " ++
-        "4__ 4g3 4__ 4F3 4__ 4g3 4__ 4e4" :: String
 
 -- ugly support code (would be less ugly if we could remove La' from
 -- BPitch, but that would mess up with too many of the early examples)
--- LetItBe.hs
+++
@@ -1,7 +0,0 @@
-module LetItBe (letItBe) where
-
-letItBe = "4g3 \
-          \6g3 2g3 4a4 4e3 4g3 4g3 4c4 4d4 \
-          \4d4 4e4 4e4 4d4 4d4 cc4 \
-          \4e4 4e4 4f4 2e4 4e4 8d4 2e4 2d4 \
-          \4d4 fc4"
